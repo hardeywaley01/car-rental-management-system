8c5896d4babb869103bf99dd2963cb6bc70420ef
hardeywaley <mtech7173@gmail.com>
2026-09-15 00:51:32 +0100

    Fix production allowed hosts

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
